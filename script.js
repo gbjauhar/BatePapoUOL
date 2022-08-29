363de7cf14176043e74b541902b794d213f6a4d0
Gabriela Jauhar <gbjauhar@gmail.com>
2022-08-29 11:31:26 -0300

    com as conversas

--- script.js
+++ script.js
@@ -1,31 +1,79 @@
+let username;
+const user = {
+    name: username
+};
 
-function iniciar(){
-    const nome = prompt("Qual seu nome?");
-    let obj = {
-        name: nome
+
+function logIn(){
+    const username = prompt("Qual seu nome?");
+}
+
+function updateChat(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(processPromise);
+}
+updateChat();
+
+function processPromise(promise){
+    console.log(promise);
+    console.log(promise.data);
+    messages = promise.data;
+    const containerMessage = document.querySelector(".containerMessage");
+    containerMessage.innerHTML = '';
+    for (let i = 0; i < messages.length; i++) {
+        if (messages[i].type === "status") {
+            containerMessage.innerHTML += `
+            <div class="messagecontent status">
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span class="message-text">${messages[i].text}</span>
+            </div>
+            `
+        }
+        if (messages[i].type === "message") {
+            containerMessage.innerHTML += `
+            <div class="messagecontent message">
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span>para</span>
+                <span class="message-to">${messages[i].to}</span>
+                <span>:</span>
+                <span class="message-text">${messages[i].text}</span>
+            </div>
+            `
+        }
+        if (messages[i].type === "private_message") {
+            containerMessage.innerHTML += `
+            <div class="messagecontent private-message">
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span>para</span>
+                <span class="message-to">${messages[i].to}</span>
+                <span>:</span>
+                <span class="message-text">${messages[i].text}</span>
+            </div>
+            `
+        }
     }
+    containerMessage.lastElementChild.scrollIntoView();
 }
-let obj;
-const envio = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", obj);
-
-envio.catch(tratarError);
-
-function tratarError(envios){
-    //while(envios.response.status !== 200){
-   // const nome = prompt("Qual seu nome?");
-//}
-
-    console.log(envios);
-}
-
-const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-promise.then(tratarSucesso);
-promise.catch(tratarErro)
-
-function tratarSucesso(mensagens){
-    console.log(mensagens.data);
-}
-
-function tratarErro(mensagens){
+function processError(promise){
     console.log("Deu errado!");
+}
+
+function sendMessage(){
+    const typedMessage = document.querySelector(".bottom input");
+    if(typedMessage.value !== ""){
+        const bodyMessage = {
+            from: user.name
+        }
+    }
+    const promiseMessage = ("https://mock-api.driven.com.br/api/v6/uol/messages", bodyMessage);
+    promiseMessage.then(updateChat);
+    promiseMessage.catch(messageError);
+    typedMessage.value = "";
+}
+
+function messageError(error){
+    window.location.reload();
 }
